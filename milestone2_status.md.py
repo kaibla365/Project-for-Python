f73b2f1fb331470d3aa66dc069900badc8e7e8ba
kaibla365 <Kaiden.blanchard@uj.edu>
2026-07-21 15:08:00 -0500

    Add files via upload

---
+++ milestone2_status.md.py
@@ -0,0 +1,19 @@
+# CS344 Final Project - milestone2_status.md.py
+# Project Title: Study session planner/ Focus Tracker
+# Kaiden Blanchard
+# Repository: https://github.com/kaibla365/Project-for-Python
+
+# Completed Features:
+# The current version now for the study session planner with focus tracker allows for a given user to enter their study
+# habits for each study session based on subject studied, the intended study time and also the focus the user had during
+# the session. The program uses user input, conditional statements, loops, and multiple functions so after each study session,
+# the program displays a summary showing the total number of study sessions, total study time, and average focus rating.
+
+# Planned Features:
+# i plan to eventually add ways to save your study sessions to like a history file tab. Load previous study session to look back on,
+# to also display insights based on subject study time. Further allow users to edit and delete any previous study inquiries.
+# Then also have a menu created with further additional options allowing for the viewing of all your study sessions and subject categorized.
+
+# Issues Encountered:
+# The hardest challenge originally I feel would be correlating all the functions into one fully automated process.
+# also would be good to better format totals within the loop and its variables for better summary statistics for it to be more accurate within each study session.
